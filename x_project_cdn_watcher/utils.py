21f06bf3085982e458f646bf25d9ad645fc6a67d
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2017-12-16 17:25:47 +0200

    update trafaret version

--- x_project_cdn_watcher/utils.py
+++ x_project_cdn_watcher/utils.py
@@ -8,9 +8,9 @@ primitive_ip_regexp = r'^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$'
 store_directory = r'^/.*/$'
 
 TRAFARET_CONF = T.Dict({
-    T.Key('host'): T.String(regex=primitive_ip_regexp),
+    T.Key('host'): T.Regexp(primitive_ip_regexp),
     T.Key('token'): T.String(),
-    T.Key('store_directory'): T.String(regex=store_directory),
+    T.Key('store_directory'): T.Regexp(store_directory),
     T.Key('port'): T.Int(),
     T.Key('mongo'): T.Any
 })
