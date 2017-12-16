ae229f1e2f3eb56bd8114af1a8b726250a46f013
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2017-12-16 17:15:06 +0200

    store in file sustem

--- x_project_cdn_watcher/routes.py
+++ x_project_cdn_watcher/routes.py
@@ -2,7 +2,7 @@ from .views import ApiFileStorageView
 
 
 def setup_routes(app):
-    app.router.add_static('/test_static', './static', show_index=True)
+    # app.router.add_static('/test_static', './static', show_index=True)
     # app.router.add_route('GET', '/{tail:.*}', ApiView)
     # app.router.add_route('HEAD', '/{tail:.*}', ApiView)
     # app.router.add_route('POST', '/{tail:.*}', ApiView)
